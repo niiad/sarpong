acc255bba4b78739f1d86547f892182fe6645b5f
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-06-16 12:56:30 +0000

    initial commit

---
+++ eduful9877/src/main/java/io/eduful9877/eduful9877/HelloController.java
@@ -0,0 +1,14 @@
+package io.eduful9877.eduful9877;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ eduful9877/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module io.eduful9877.eduful9877 {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens io.eduful9877.eduful9877 to javafx.fxml;
+    exports io.eduful9877.eduful9877;
+}
