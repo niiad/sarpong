a1b58f4bce1fabcf7f30cbea1f8e9d5fd9b7ac4b
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-06-16 13:03:44 +0000

    deleted unnecessary files

--- eduful9877/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module io.eduful9877.eduful9877 {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens io.eduful9877.eduful9877 to javafx.fxml;
-    exports io.eduful9877.eduful9877;
-}
