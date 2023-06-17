617f45d28266dd4b1b69662dbaa076a035ae0afe
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-06-17 00:59:26 +0000

    enabled scenebuilder kit in intellij

--- kelvin-eduful/src/main/java/com/eduful9788/kelvineduful/HelloController.java
+++ kelvin-eduful/src/main/java/com/eduful9788/kelvineduful/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText ( "Welcome to JavaFX Application!" );
+        welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
