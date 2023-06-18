30b541992be4713b8b6d95ca548d84702cc0b2bf
Iamkweku <100833393+Iamkweku@users.noreply.github.com>
2023-06-18 15:32:15 +0000

    Delete Pachomius Kweku Lawson directory

--- Pachomius Kweku Lawson/Pachomius_lawson_10904950/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.pachomius_lawson_10904950 {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                        requires org.kordamp.bootstrapfx.core;
-            
-    opens com.example.pachomius_lawson_10904950 to javafx.fxml;
-    exports com.example.pachomius_lawson_10904950;
-}
