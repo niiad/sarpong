9a15ec26342a05ff8b7b8e5e57c630cffa93be04
Nii Adotei <61387678+niiadoteibrown@users.noreply.github.com>
2023-06-20 23:14:11 +0000

    Delete David_Boateng directory

--- David_Boateng/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.david_boateng {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.david_boateng to javafx.fxml;
-    exports com.example.david_boateng;
-}
