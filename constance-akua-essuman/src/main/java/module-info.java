40cae4d7be293b0e28cdc0e9782d30a4ccfa026a
Nii Adotei <61387678+niiadoteibrown@users.noreply.github.com>
2023-06-20 23:19:03 +0000

    Delete constance-akua-essuman directory

--- constance-akua-essuman/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.essuman3020.constanceakuaessuman {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.essuman3020.constanceakuaessuman to javafx.fxml;
-    exports com.essuman3020.constanceakuaessuman;
-}
