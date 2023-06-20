e69b9b2c15d8969ad3332a3fb179508122d39fd2
Nii Adotei <61387678+niiadoteibrown@users.noreply.github.com>
2023-06-20 23:14:48 +0000

    Delete Gideon Agbosu directory

--- Gideon Agbosu/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.gideonredemeer.awesomgideon {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.gideonredemeer.awesomgideon to javafx.fxml;
-    exports com.gideonredemeer.awesomgideon;
-}
