b6ce701516eee7d21934d0738e14242acef4fcf3
Djemm20 <105044342+Djemm20@users.noreply.github.com>
2023-06-20 11:09:17 +0000

    Add files via upload

---
+++ Emmanuel Kodjo Djangmah/emmanuelkodjodjangmah/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.emmanuelkodjodjangmah {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.emmanuelkodjodjangmah to javafx.fxml;
+    exports com.example.emmanuelkodjodjangmah;
+}
