af2f4313f478515024c0912486e8d5adb7505653
Nii Adotei <61387678+niiadoteibrown@users.noreply.github.com>
2023-06-20 23:17:02 +0000

    Delete Opoku Tracy directory

--- Opoku Tracy/Tracy Opoku/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.tracyopoku {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.tracyopoku to javafx.fxml;
-    exports com.example.tracyopoku;
-}
