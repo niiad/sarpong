e5ccc3c7e2171c44270d473152e8eb6e14c24613
Nii Adotei <61387678+niiadoteibrown@users.noreply.github.com>
2023-06-20 23:17:17 +0000

    Delete Pachomius Kweku Lawson directory

--- Pachomius Kweku Lawson/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.project {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.project to javafx.fxml;
-    exports com.example.project;
-}
