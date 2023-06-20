a5d874cb15de1e1218cd5201b7e3572e7ebfeb77
Nii Adotei <61387678+niiadoteibrown@users.noreply.github.com>
2023-06-20 23:08:49 +0000

    Delete Emmanuel Kodjo Djangmah directory

--- Emmanuel Kodjo Djangmah/emmanuelkodjodjangmah/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.emmanuelkodjodjangmah {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.emmanuelkodjodjangmah to javafx.fxml;
-    exports com.example.emmanuelkodjodjangmah;
-}
