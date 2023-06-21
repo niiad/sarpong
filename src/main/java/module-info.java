54134c531a057eddf261f2439bc1d831e4a779ac
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-06-21 20:37:39 +0000

    deleted project created with the repo

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.sarkseven {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.sarkseven to javafx.fxml;
-    exports com.example.sarkseven;
-}
