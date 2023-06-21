a657aaaa2ada498c67c4bbdec76f2a096ca094e7
user <user@MacBook-Pro.local>
2023-06-21 07:53:26 +0000

    initial commit

---
+++ caessuman/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.essuman3020.constanceakuaessuman {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.essuman3020.constanceakuaessuman to javafx.fxml;
+    exports com.essuman3020.constanceakuaessuman;
+}
