ce9258ae99db9becdb29580399621782a905644e
Adubee <82320818+Adubee@users.noreply.github.com>
2023-06-21 13:03:29 +0000

    Delete kwasiAduBediako/project directory

--- kwasiAduBediako/project/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.kwasi_adubediako {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.kwasi_adubediako to javafx.fxml;
-    exports com.example.kwasi_adubediako;
-}
