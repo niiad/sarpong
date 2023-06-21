aa1b8d292147ae749e3db239d33d1bd2aadae767
Ashiangmor Caleb <56304597+Kuameh@users.noreply.github.com>
2023-06-21 05:00:33 +0000

    🎉 initial commit for ashiangmor

---
+++ ashiangmor/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.ashiangmor.ashiangmor {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.ashiangmor.app to javafx.fxml;
+    exports com.ashiangmor.app;
+}
