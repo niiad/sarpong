987cddc552b7e14e4f83c1e55b1f1d67180bb065
OdensCode <108490688+OdensCode@users.noreply.github.com>
2023-06-21 00:15:35 +0000

    Delete JerryPaddy directory

--- JerryPaddy/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.jerrypaddy {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.jerrypaddy to javafx.fxml;
-    exports com.example.jerrypaddy;
-}
