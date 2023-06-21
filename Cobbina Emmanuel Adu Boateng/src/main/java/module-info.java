a599fb526e70545be5d0cfb36e4802f95d62cfd5
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-06-21 20:44:43 +0000

    Delete Cobbina Emmanuel Adu Boateng directory

--- Cobbina Emmanuel Adu Boateng/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.cobbin_emmanuel_adu_boateng {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.cobbin_emmanuel_adu_boateng to javafx.fxml;
-    exports com.example.cobbin_emmanuel_adu_boateng;
-}
