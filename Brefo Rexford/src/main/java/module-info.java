78a0c7a166572951b720fdf4647676f0ed047055
Nii Adotei <61387678+niiadoteibrown@users.noreply.github.com>
2023-06-21 23:59:42 +0000

    Delete Brefo Rexford directory
    check #3

--- Brefo Rexford/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.brefo_rexford {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.brefo_rexford to javafx.fxml;
-    exports com.example.brefo_rexford;
-}
