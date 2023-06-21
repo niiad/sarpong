c6940f0553d28817a2a3be9d0791aca43d954812
sonnysam <samuelagbenyo067@gmail.com>
2023-06-21 20:54:11 +0000

    student id and name added

--- samuelagbenyo/src/main/java/com/example/samuelagbenyo/HelloApplication.java
+++ samuelagbenyo/src/main/java/com/example/samuelagbenyo/HelloApplication.java
@@ -1,5 +1,8 @@
 package com.example.samuelagbenyo;
 
+// AGBENYO SAMUEL SELORM KWAME
+// 10913415
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
