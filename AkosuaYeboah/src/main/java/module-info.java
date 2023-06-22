e16d3ed3c0c2ee813f8a5aab8d46426b9740360f
Nii Adotei <61387678+niiadoteibrown@users.noreply.github.com>
2023-06-22 00:04:47 +0000

    Delete AkosuaYeboah directory
    check #3

--- AkosuaYeboah/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.akosuayeboah {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.akosuayeboah to javafx.fxml;
-    exports com.example.akosuayeboah;
-}
