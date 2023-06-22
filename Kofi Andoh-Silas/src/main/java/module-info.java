2359663f4ecd3d23a4c73ab3b78a2ca8c33734a3
Nii Adotei <61387678+niiadoteibrown@users.noreply.github.com>
2023-06-22 00:03:43 +0000

    Delete Kofi Andoh-Silas directory
    check #3

--- Kofi Andoh-Silas/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.kofiandohsilas {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.kofiandohsilas to javafx.fxml;
-    exports com.example.kofiandohsilas;
-}
