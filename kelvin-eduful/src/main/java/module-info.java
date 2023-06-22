b49dbfce587f3156d6c8297f8fe9d565b5b6bae8
Nii Adotei <61387678+niiadoteibrown@users.noreply.github.com>
2023-06-22 00:02:02 +0000

    Delete kelvin-eduful directory
    Check #3

--- kelvin-eduful/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.eduful9788.kelvineduful {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.eduful9788.kelvineduful to javafx.fxml;
-    exports com.eduful9788.kelvineduful;
-}
