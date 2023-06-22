a7838743ed97b8b4a79bc3d31894178e9ee9653f
Nii Adotei <61387678+niiadoteibrown@users.noreply.github.com>
2023-06-22 00:05:36 +0000

    Delete JarawuraVincensiaWelagamu directory
    check #3

--- JarawuraVincensiaWelagamu/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.jarawuravincensiawelagamu {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.jarawuravincensiawelagamu to javafx.fxml;
-    exports com.example.jarawuravincensiawelagamu;
-}
