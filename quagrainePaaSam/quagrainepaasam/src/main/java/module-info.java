fe45d78a614409e4c8beb92fb147780999896f7a
QPaaSam <qaugustine08@gmail.com>
2023-06-25 15:37:38 +0000

    Delete quagrainePaaSam directory

--- quagrainePaaSam/quagrainepaasam/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.quagrainepaasam {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.quagrainepaasam to javafx.fxml;
-    exports com.example.quagrainepaasam;
-}
