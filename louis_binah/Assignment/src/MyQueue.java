2c466aaa304aba5b3091bf0a9da3f982d7718791
visteen <visteen192@gmail.com>
2023-06-25 17:27:22 +0000

    first commit

---
+++ louis_binah/Assignment/src/MyQueue.java
@@ -0,0 +1,29 @@
+import java.util.Queue;
+import java.util.LinkedList;
+public class MyQueue {
+    private Queue<String> queue;
+
+    public MyQueue(){
+        queue = new LinkedList<>();
+    }
+    public void enqueue(String element){
+        queue.add(element);
+    }
+    public String dequeue(){
+        return queue.poll() ;
+    }
+    public String peek(){
+        return queue.peek();
+    }
+    public boolean IsEmpty(){
+        return queue.isEmpty();
+    }
+    public int Size(){
+        return queue.size();
+    }
+    @Override
+    public  String toString(){
+        return queue.toString();
+    }
+}
+
