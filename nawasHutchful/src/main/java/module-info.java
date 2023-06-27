212f949220d4f8ea43c09c93a01a3ac817dde0a3
Nassymple <100800574+Nassymple@users.noreply.github.com>
2023-06-27 13:17:41 +0000

    Project folder upload

---
+++ nawasHutchful/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.nawashutchful {
+    requires javafx.controls;
+    requires javafx.fxml;
+        requires javafx.web;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            requires net.synedra.validatorfx;
+            requires org.kordamp.ikonli.javafx;
+            requires org.kordamp.bootstrapfx.core;
+            requires eu.hansolo.tilesfx;
+            requires com.almasb.fxgl.all;
+    
+    opens com.example.nawashutchful to javafx.fxml;
+    exports com.example.nawashutchful;
+}
