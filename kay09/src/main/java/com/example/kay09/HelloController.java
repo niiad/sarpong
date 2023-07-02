0bcd9a66cca96ffc979e72dc5a7492b1300d5359
Kay09xx <nanak.antwi09@gmail.com>
2023-07-02 21:14:16 +0000

    hello

---
+++ kay09/src/main/java/com/example/kay09/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.kay09;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
