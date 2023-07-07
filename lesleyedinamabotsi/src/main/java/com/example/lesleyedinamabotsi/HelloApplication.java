4db665679e979fdc7e4d4af3eb422af65f07ec21
KHEVLAR <lesley.edinam@gmail.com>
2023-07-07 15:32:04 +0000

    Inventory Project Init Commit

---
+++ lesleyedinamabotsi/src/main/java/com/example/lesleyedinamabotsi/HelloApplication.java
@@ -0,0 +1,25 @@
+package com.example.lesleyedinamabotsi;
+/* *
+@author Lesley Edinam Kwasi Abotsi (10862257)
+* */
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
