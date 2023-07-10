07afa9c0b128f69f88af4142b10c93c6d11d0692
Nii Adotei <samuelbrownai@gmail.com>
2023-07-10 20:52:16 +0000

    user interface sample

--- niiadotei/src/main/java/module-info.java
+++ niiadotei/src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.example.niiadotei {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.niiadotei to javafx.fxml;
     exports com.example.niiadotei;
+    exports com.example.niiadotei.controller;
+    opens com.example.niiadotei.controller to javafx.fxml;
 }
