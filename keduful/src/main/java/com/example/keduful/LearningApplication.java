a72297610380d63ad2d90aa18d396246339e4650
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-07-13 18:20:25 +0000

    added data

--- keduful/src/main/java/com/example/keduful/LearningApplication.java
+++ keduful/src/main/java/com/example/keduful/LearningApplication.java
@@ -11,10 +11,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class LearningApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(LearningApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(),320,240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
