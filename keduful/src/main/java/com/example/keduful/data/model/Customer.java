099019d232ce33f3e33c867f99ded4f0611b34d5
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-07-13 18:46:38 +0000

    implemented logic for find and findALL

--- keduful/src/main/java/com/example/keduful/data/model/Customer.java
+++ keduful/src/main/java/com/example/keduful/data/model/Customer.java
@@ -2,4 +2,12 @@ package com.example.keduful.data.model;
 
 public class Customer {
     private int id;
+
+    Customer() {
+        id = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
--- keduful/src/main/java/com/example/keduful/repository/ProductRepository.java
+++ keduful/src/main/java/com/example/keduful/repository/ProductRepository.java
@@ -5,18 +5,25 @@ import com.example.keduful.data.model.Product;
 import java.util.List;
 
 public class ProductRepository implements Repository<Product> {
+
+    List<Product> products;
     @Override
     public void save(Product entity) {
-
+        //TODO: will implement as soon as database connectivity is established
     }
 
     @Override
     public Product find(int id) {
+        for (Product product: products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
         return null;
     }
 
     @Override
     public List<Product> findAll() {
-        return null;
+        return products;
     }
 }
