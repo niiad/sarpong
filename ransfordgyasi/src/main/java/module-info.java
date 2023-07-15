21302c5dc479af419fa8c2591e203bf7a2ff2a4b
Ransford Gyasi <101061232+ckbcodes@users.noreply.github.com>
2023-07-15 21:38:17 +0000

    Revert "Add files via upload"

--- ransfordgyasi/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.ransfordgyasi {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.ransfordgyasi to javafx.fxml;
-    exports com.example.ransfordgyasi;
-}
