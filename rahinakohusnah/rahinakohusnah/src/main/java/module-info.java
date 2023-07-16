54e81c01a923cfe26c2fc34ebf6b25b5aeb8c374
Rahinat03 <108794378+Rahinat03@users.noreply.github.com>
2023-07-16 12:41:22 +0000

    Delete rahinakohusnah directory

--- rahinakohusnah/rahinakohusnah/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.rahinakohusnah {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.rahinakohusnah to javafx.fxml;
-    exports com.example.rahinakohusnah;
-}
