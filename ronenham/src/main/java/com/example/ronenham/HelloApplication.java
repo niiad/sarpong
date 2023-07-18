63fd6ca3ee706c1b97db3d88dc1aa244f2ae4888
RonenTGreat <ronenh53@gmail.com>
2023-07-18 19:55:50 +0000

    added name and id

--- ronenham/src/main/java/com/example/ronenham/HelloApplication.java
+++ ronenham/src/main/java/com/example/ronenham/HelloApplication.java
@@ -12,6 +12,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+
+/*
+* Ronen Nii Ayi Hammond
+* 10899320
+* */
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
