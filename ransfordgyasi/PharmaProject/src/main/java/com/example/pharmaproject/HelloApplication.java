07dcf928a918a1a8a35d31af840d1fc01b1c26d8
Ransford Gyasi <101061232+ckbcodes@users.noreply.github.com>
2023-07-18 12:56:11 +0000

    Add files via upload

--- ransfordgyasi/PharmaProject/src/main/java/com/example/pharmaproject/HelloApplication.java
+++ ransfordgyasi/PharmaProject/src/main/java/com/example/pharmaproject/HelloApplication.java
@@ -8,11 +8,16 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 720, 480);
+
+        // Apply the CSS
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+
+        stage.setTitle("RansPharma");
         stage.setScene(scene);
         stage.show();
     }
