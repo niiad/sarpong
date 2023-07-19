f8efd918b91b03fabd6ba4e484eeb27326098166
Nathan Eyram <dtwum1738@gmail.com>
2023-07-19 15:19:20 +0000

    Initial commit

---
+++ kelvinKizzitoKwasigah/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.kelvinkizzitokwasigah {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.kelvinkizzitokwasigah to javafx.fxml;
+    exports com.example.kelvinkizzitokwasigah;
+}
