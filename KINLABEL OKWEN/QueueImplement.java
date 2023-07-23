95924e16494dbbe246a55a534cf2dc00197abc22
MIYAKA <75727824+BambinoRiya@users.noreply.github.com>
2023-07-23 14:52:52 +0000

    Create QueueImplement.java

---
+++ KINLABEL OKWEN/QueueImplement.java
@@ -0,0 +1,45 @@
+//*******************************************************************
+// Customer.java Java Foundations
+//
+// Represents a waiting customer.
+//*******************************************************************
+public class Customer
+{
+private int arrivalTime, departureTime;
+//-----------------------------------------------------------------
+// Creates a new customer with the specified arrival time.
+//-----------------------------------------------------------------
+public Customer (int arrives)
+{
+arrivalTime = arrives;
+departureTime = 0;
+}
+//-----------------------------------------------------------------
+// Returns the arrival time of this customer.
+//-----------------------------------------------------------------
+public int getArrivalTime()
+{
+return arrivalTime;
+}
+//-----------------------------------------------------------------
+// Sets the departure time for this customer.
+//-----------------------------------------------------------------
+public void setDepartureTime (int departs)
+{
+departureTime = departs;
+}
+//-----------------------------------------------------------------
+// Returns the departure time of this customer.
+//-----------------------------------------------------------------
+public int getDepartureTime()
+{
+return departureTime;
+}
+//-----------------------------------------------------------------
+// Computes and returns the total time spent by this customer.
+//-----------------------------------------------------------------
+public int totalTime()
+{
+return departureTime - arrivalTime;
+}
+}
